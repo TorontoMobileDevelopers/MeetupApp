91856b35ebaa496287a6cf2f131c47aed299d90b
Dylan Berry <dylanberry@gmail.com>
2018-05-19 17:56:00 -0400

    Css, basepage, initial work on main page styling

---
+++ MeetupApp/MeetupApp/MeetupApp/Views/BasePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MeetupApp.Views
+{
+    public partial class BasePage : ContentPage
+    {
+        public BasePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MeetupApp/MeetupApp/MeetupApp/Views/MainPage.xaml.cs
+++ MeetupApp/MeetupApp/MeetupApp/Views/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace MeetupApp.Views
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : BasePage
 	{
 		public MainPage ()
 		{
