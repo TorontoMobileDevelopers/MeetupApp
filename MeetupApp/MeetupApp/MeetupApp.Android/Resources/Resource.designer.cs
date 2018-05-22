b3f5c93da725074ffd0c4309c88dcb57bc0eb229
Dylan Berry <dylanberry@gmail.com>
2018-05-22 13:37:02 -0400

    More styling

--- MeetupApp/MeetupApp/MeetupApp.Android/Resources/Resource.designer.cs
+++ MeetupApp/MeetupApp/MeetupApp.Android/Resources/Resource.designer.cs
@@ -3623,7 +3623,13 @@ namespace MeetupApp.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int ic_launcher = 2130903040;
+			public const int Backgroundmdpi = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int ic_launcher = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int MobileDev = 2130903042;
 			
 			static Mipmap()
 			{
