e3ebe989811edc870406976719dd9616ff964e4a
Daniel Causer <causerd@fcc.ca>
2018-07-27 20:20:45 -0400

    [DC] Swipable ios Tabbed page.

---
+++ MeetupApp/MeetupApp/MeetupApp.iOS/Renderers/SwipeTabbedRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using CoreAnimation;
+using CoreGraphics;
+using MeetupApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(SwipeTabbedRenderer))]
+public class SwipeTabbedRenderer : TabbedRenderer
+{
+    public override void ViewWillAppear(bool animated)
+    {
+        base.ViewWillAppear(animated);
+
+        NativeView.AddGestureRecognizer(new UISwipeGestureRecognizer(() => SelectNextTab(1)) { Direction = UISwipeGestureRecognizerDirection.Left, ShouldRecognizeSimultaneously = ShouldRecognizeSimultaneously });
+        NativeView.AddGestureRecognizer(new UISwipeGestureRecognizer(() => SelectNextTab(-1)) { Direction = UISwipeGestureRecognizerDirection.Right, ShouldRecognizeSimultaneously = ShouldRecognizeSimultaneously });
+    }
+
+    void SelectNextTab(int direction)
+    {
+        int nextIndex = TabbedPage.GetIndex(Tabbed.CurrentPage) + direction;
+        if (nextIndex < 0 || nextIndex >= Tabbed.Children.Count) return;
+        var nextPage = Tabbed.Children[nextIndex];
+        UIView.Transition(Platform.GetRenderer(Tabbed.CurrentPage).NativeView, Platform.GetRenderer(nextPage).NativeView, 0.15, UIViewAnimationOptions.TransitionCrossDissolve, null);
+        Tabbed.CurrentPage = nextPage;
+    }
+
+    static bool ShouldRecognizeSimultaneously(UIGestureRecognizer gestureRecognizer, UIGestureRecognizer otherGestureRecognizer) => gestureRecognizer != otherGestureRecognizer;
+}
